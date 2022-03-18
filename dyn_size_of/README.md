e7559beff07ebee8e4806e36d9847ca68aab2a12
Piotr Beling <piotr.beling@wmii.uni.lodz.pl>
2022-03-18 11:37:50 +0100

    dyn_size_mem: support for tuples, improved doc.

--- dyn_size_of/README.md
+++ dyn_size_of/README.md
@@ -1,14 +1,52 @@
 `dyn_size_of` is the Rust library by Piotr Beling to report approximate amount of memory consumed by variables,
 including the memory allocated on heap.
 
-Please note that the library has very limited capabilities and is mainly developed for my other projects.
+# Examples
 
-# Example
-
+## Simple usage
 ```rust
 use dyn_size_of::GetSize;
 
 let bs = vec![1u32, 2u32, 3u32].into_boxed_slice();
 assert_eq!(bs.size_bytes_dyn(), 3*4);
 assert_eq!(bs.size_bytes(), 3*4 + std::mem::size_of_val(&bs));
+```
+
+## Implementing GetSize for a custom type
+```rust
+use dyn_size_of::GetSize;
+
+struct NoHeapMem {
+    a: u32,
+    b: u8
+}
+
+// default implementation is fine for types that do not use heap allocations
+impl GetSize for NoHeapMem {}
+
+struct WithHeapMem {
+    a: Vec<u32>,
+    b: Vec<u8>,
+    c: u32
+}
+
+// For types that use heap allocations:
+impl GetSize for WithHeapMem {
+    // size_bytes_dyn must be implemented and return amount of heap memory used
+    fn size_bytes_dyn(&self) -> usize {
+        self.a.size_bytes_dyn() + self.b.size_bytes_dyn()
+    }
+    // USES_DYN_MEM must be set to true
+    const USES_DYN_MEM: bool = true;
+}
+
+let s = NoHeapMem { a: 1, b: 2 };
+assert_eq!(NoHeapMem::USES_DYN_MEM, false);
+assert_eq!(s.size_bytes_dyn(), 0);
+assert_eq!(s.size_bytes(), std::mem::size_of_val(&s));
+
+let d = WithHeapMem { a: vec![1, 2], b: vec![3, 4], c: 5 };
+assert_eq!(WithHeapMem::USES_DYN_MEM, true);
+assert_eq!(d.size_bytes_dyn(), 2*4 + 2*1);
+assert_eq!(d.size_bytes(), 2*4 + 2*1 + std::mem::size_of_val(&d));
 ```
--- dyn_size_of/src/lib.rs
+++ dyn_size_of/src/lib.rs
@@ -2,12 +2,12 @@
 
 /// Provides methods to get dynamic and total size of the variable.
 pub trait GetSize {
-    /// Returns approximate number of bytes occupied by dynamic part of `self`.
+    /// Returns approximate number of bytes occupied by dynamic (heap) part of `self`.
     /// Same as `self.size_bytes() - std::mem::size_of_val(self)`.
-    fn size_bytes_dyn(&self) -> usize { 0 }
+    #[inline] fn size_bytes_dyn(&self) -> usize { 0 }
 
     /// Returns approximate, total (including heap memory) number of bytes occupied by `self`.
-    fn size_bytes(&self) -> usize {
+    #[inline] fn size_bytes(&self) -> usize {
         std::mem::size_of_val(self) + self.size_bytes_dyn()
     }
 
@@ -16,7 +16,7 @@ pub trait GetSize {
 }
 
 macro_rules! impl_nodyn_getsize_for {
-    ($x:ty) => (impl GetSize for $x {});
+    ($x:ty) => (impl self::GetSize for $x {});
     // `$x` followed by at least one `$y,`
     ($x:ty, $($y:ty),+) => (
         impl self::GetSize for $x {}
@@ -24,26 +24,25 @@ macro_rules! impl_nodyn_getsize_for {
     )
 }
 
-impl_nodyn_getsize_for!(u8, u16, u32, u64, u128, i8, i16, i32, i64, i128, f32, f64, char, ());
+impl_nodyn_getsize_for!(u8, u16, u32, u64, u128, i8, i16, i32, i64, i128, f32, f64, char, isize, usize, ());
 
 //impl<T: GetSize> GetSize for [T] {    // this works also with slices, but is this sound?
 impl<T: GetSize, const N: usize> GetSize for [T; N] {
     fn size_bytes_dyn(&self) -> usize {
         if T::USES_DYN_MEM {
-            self.iter().map(|i| i.size_bytes_dyn()).sum()
+            self.iter().map(self::GetSize::size_bytes_dyn).sum()
         } else {
             0
         }
     }
-    //fn can_use_dyn_mem() -> bool { T::can_use_dyn_mem() }
     const USES_DYN_MEM: bool = T::USES_DYN_MEM;
 }
 
 macro_rules! impl_dyn_getsize_methods {
     ($T:ty) => (
-        fn size_bytes_dyn(&self) -> usize {
+        fn size_bytes_dyn(&self) -> ::std::primitive::usize {
             if <$T>::USES_DYN_MEM {
-                self.iter().map(|i| i.size_bytes()).sum()
+                self.iter().map(self::GetSize::size_bytes).sum()
             } else {
                 ::std::mem::size_of::<$T>() * self.len()
             }
@@ -60,7 +59,7 @@ impl<T: GetSize> GetSize for Vec<T> {
     fn size_bytes_dyn(&self) -> usize {
         let c = ::std::mem::size_of::<T>() * self.capacity();
         if T::USES_DYN_MEM {
-            c + self.iter().map(|i| i.size_bytes_dyn()).sum::<usize>()
+            c + self.iter().map(GetSize::size_bytes_dyn).sum::<usize>()
         } else {
             c
         }
@@ -68,8 +67,29 @@ impl<T: GetSize> GetSize for Vec<T> {
     const USES_DYN_MEM: bool = true;
 }
 
+macro_rules! impl_getsize_for_tuple {
+    ($( $T:ident ),+) => {
+        impl<$( $T: self::GetSize ),+> self::GetSize for ($( $T, )+) {
+            #[allow(non_snake_case)]
+            fn size_bytes_dyn(&self) -> ::std::primitive::usize {
+                let &($( ref $T, )+) = self;
+                0 $( + $T.size_bytes_dyn() )+
+            }
+            const USES_DYN_MEM: bool = $( $T::USES_DYN_MEM )|*;
+        }
+    }
+}
 
-
+impl_getsize_for_tuple!(A);
+impl_getsize_for_tuple!(A, B);
+impl_getsize_for_tuple!(A, B, C);
+impl_getsize_for_tuple!(A, B, C, D);
+impl_getsize_for_tuple!(A, B, C, D, E);
+impl_getsize_for_tuple!(A, B, C, D, E, F);
+impl_getsize_for_tuple!(A, B, C, D, E, F, G);
+impl_getsize_for_tuple!(A, B, C, D, E, F, G, H);
+impl_getsize_for_tuple!(A, B, C, D, E, F, G, H, I);
+impl_getsize_for_tuple!(A, B, C, D, E, F, G, H, I, J);
 
 #[cfg(test)]
 mod tests {
@@ -100,6 +120,7 @@ mod tests {
         assert_eq!(vec![[1u32, 2u32], [3u32, 4u32]].size_bytes_dyn(), 4*4);
         let v = vec![1u32, 2u32];
         assert_eq!(vec![v.clone(), v.clone()].size_bytes_dyn(), 2*v.size_bytes());
+        assert_eq!(Vec::<u32>::with_capacity(3).size_bytes_dyn(), 3*4);
     }
 
     #[test]
@@ -107,5 +128,12 @@ mod tests {
         let bs = vec![1u32, 2u32, 3u32].into_boxed_slice();
         assert_eq!(bs.size_bytes_dyn(), 3*4);
         assert_eq!(bs.size_bytes(), 3*4 + std::mem::size_of_val(&bs));
+    }
+
+    #[test]
+    fn test_tuple() {
+        assert_eq!((1u32, 2u32).size_bytes_dyn(), 0);
+        assert_eq!((1u32, vec![3u32, 4u32]).size_bytes_dyn(), 2*4);
+        assert_eq!((vec![1u32, 2u32], vec![3u32, 4u32]).size_bytes_dyn(), 4*4);
     }
 }
