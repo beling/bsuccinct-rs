742182905655a95011cc51b3d749490b04fd8002
Piotr Beling <piotr.beling@wmii.uni.lodz.pl>
2022-11-13 14:38:55 +0100

    renaming iterator over bit ones to BitOnesIterator

--- bitm/src/bitvec.rs
+++ bitm/src/bitvec.rs
@@ -2,13 +2,13 @@ use std::iter::FusedIterator;
 use super::{ceiling_div, n_lowest_bits};
 
 /// Iterator over bits set to one in slice of `u64`.
-pub struct OneIndicesIterator<'a> {
+pub struct BitOnesIterator<'a> {
     segment_iter: std::slice::Iter<'a, u64>,
     first_segment_bit: usize,
     current_segment: u64
 }
 
-impl<'a> OneIndicesIterator<'a> {
+impl<'a> BitOnesIterator<'a> {
     pub fn new(slice: &'a [u64]) -> Self {
         let mut segment_iter = slice.into_iter();
         let current_segment = segment_iter.next().copied().unwrap_or(0);
@@ -20,7 +20,7 @@ impl<'a> OneIndicesIterator<'a> {
     }
 }
 
-impl<'a> Iterator for OneIndicesIterator<'a> {
+impl<'a> Iterator for BitOnesIterator<'a> {
     type Item = usize;
 
     fn next(&mut self) -> Option<Self::Item> {
@@ -39,13 +39,13 @@ impl<'a> Iterator for OneIndicesIterator<'a> {
     }
 }
 
-impl<'a> ExactSizeIterator for OneIndicesIterator<'a> {
+impl<'a> ExactSizeIterator for BitOnesIterator<'a> {
     #[inline] fn len(&self) -> usize {
-        self.current_segment.count_ones() as usize + self.segment_iter.as_slice().count_ones()
+        self.current_segment.count_ones() as usize + self.segment_iter.as_slice().count_bit_ones()
     }
 }
 
-impl<'a> FusedIterator for OneIndicesIterator<'a> where std::slice::Iter<'a, u64>: FusedIterator {}
+impl<'a> FusedIterator for BitOnesIterator<'a> where std::slice::Iter<'a, u64>: FusedIterator {}
 
 /// The trait that is implemented for the array of `u64` and extends it with methods for
 /// accessing and modifying single bits or arbitrary fragments consisted of few (up to 63) bits.
@@ -66,13 +66,13 @@ pub trait BitAccess {
     fn set_bits(&mut self, begin: usize, v: u64, len: u8);
 
     /// Returns the number of zeros (cleared bits).
-    fn count_zeros(&self) -> usize;
+    fn count_bit_zeros(&self) -> usize;
 
     /// Returns the number of ones (set bits).
-    fn count_ones(&self) -> usize;
+    fn count_bit_ones(&self) -> usize;
 
     /// Returns iterator over indices of ones (set bits).
-    fn one_indices(&self) -> OneIndicesIterator;
+    fn bit_ones(&self) -> BitOnesIterator;
 
     /// Gets `v_size` bits with indices in range [`index*v_size`, `index*v_size+v_size`).
     #[inline(always)] fn get_fragment(&self, index: usize, v_size: u8) -> u64 {
@@ -220,16 +220,16 @@ impl BitAccess for [u64] {
         self[bit_nr / 64] &= !((1u64) << (bit_nr % 64) as u64);
     }
 
-    fn count_zeros(&self) -> usize {
+    fn count_bit_zeros(&self) -> usize {
         self.into_iter().map(|s| s.count_zeros() as usize).sum()
     }
 
-    fn count_ones(&self) -> usize {
+    fn count_bit_ones(&self) -> usize {
         self.into_iter().map(|s| s.count_ones() as usize).sum()
     }
 
-    #[inline(always)] fn one_indices(&self) -> OneIndicesIterator {
-        OneIndicesIterator::new(self)
+    #[inline(always)] fn bit_ones(&self) -> BitOnesIterator {
+        BitOnesIterator::new(self)
     }
 
     fn get_bits(&self, begin: usize, len: u8) -> u64 {
@@ -415,13 +415,13 @@ mod tests {
     fn bits() {
         let mut b = Box::<[u64]>::with_filled_64bit_segments(2);
         assert_eq!(b.as_ref(), [u64::MAX, u64::MAX]);
-        assert_eq!(b.count_ones(), 128);
-        assert_eq!(b.count_zeros(), 0);
+        assert_eq!(b.count_bit_ones(), 128);
+        assert_eq!(b.count_bit_zeros(), 0);
         assert!(b.get_bit(3));
         assert!(b.get_bit(73));
         b.clear_bit(73);
-        assert_eq!(b.count_ones(), 127);
-        assert_eq!(b.count_zeros(), 1);
+        assert_eq!(b.count_bit_ones(), 127);
+        assert_eq!(b.count_bit_zeros(), 1);
         assert!(!b.get_bit(73));
         assert!(b.get_bit(72));
         assert!(b.get_bit(74));
@@ -432,7 +432,7 @@ mod tests {
     #[test]
     fn iterators() {
         let b = [0b101u64, 0b10u64];
-        let mut ones = b.one_indices();
+        let mut ones = b.bit_ones();
         assert_eq!(ones.len(), 3);
         assert_eq!(ones.next(), Some(0));
         assert_eq!(ones.len(), 2);
