3fb409190242c1136c29dc075867876a86a2acac
Piotr Beling <piotr.beling@wmii.uni.lodz.pl>
2023-01-18 12:41:59 +0100

    indexing2 is public

--- ph/src/fp/mod.rs
+++ ph/src/fp/mod.rs
@@ -1,7 +1,7 @@
 mod hash;
 pub use hash::{FPHash, FPHashConf};
 
-mod indexing2;
+pub mod indexing2;
 pub use indexing2::{GroupSize, SeedSize, TwoToPowerBits, TwoToPowerBitsStatic, Bits, Bits8};
 mod hash2;
 pub use hash2::{FPHash2, FPHash2Conf, FPHash2Builder};
