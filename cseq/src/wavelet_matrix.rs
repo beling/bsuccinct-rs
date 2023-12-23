1bef2dc77445a7d94d783e08aa2f1f5a460b1ef5
Piotr Beling <piotr.beling@wmii.uni.lodz.pl>
2023-12-23 13:42:16 +0100

    doc. and refactoring

--- cseq/src/wavelet_matrix.rs
+++ cseq/src/wavelet_matrix.rs
@@ -4,15 +4,15 @@ use bitm::{BitAccess, BitVec, ArrayWithRankSelect101111, CombinedSampling, Rank,
 use dyn_size_of::GetSize;
 
 /// Constructs bit vectors for the (current) level of velvet matrix.
-/// Stores bits of `lower_bits` values from previous level in to vectors:
-/// - `upper_bit` stores the most significant bits (msb; shown by `upper_bit_mask`) of the subsequent values,
+/// Stores bits of `lower_bits` items from previous level in to vectors:
+/// - `upper_bit` stores the most significant bits (msb; shown by `upper_bit_mask`) of the subsequent items,
 ///     in the order from previous level,
-/// - `lower_bits` stores all (`bits_per_value`) less significant bits (shown by `upper_bit_mask-1`)
-///     of the subsequent values, stable sorted by most significant bits (msb).
-/// The following values show the bit indices to insert the next value:
+/// - `lower_bits` stores all (`bits_per_item`) less significant bits (shown by `upper_bit_mask-1`)
+///     of the subsequent items, stable sorted by most significant bits (msb).
+/// The following items show the bit indices to insert the next item:
 /// - `upper_index` is index of `upper_bit`,
-/// - `lower_zero_index` is index of `lower_bits` to insert next value with 0 msb,
-/// - `lower_one_index` is index of `lower_bits` to insert next value with 1 msb,
+/// - `lower_zero_index` is index of `lower_bits` to insert next item with 0 msb,
+/// - `lower_one_index` is index of `lower_bits` to insert next item with 1 msb,
 struct LevelBuilder {
     upper_bit: Box<[u64]>,
     upper_index: usize,
@@ -20,7 +20,7 @@ struct LevelBuilder {
     lower_zero_index: usize,
     lower_one_index: usize,
     upper_bit_mask: u64,
-    bits_per_value: u8
+    bits_per_item: u8
 }
 
 impl LevelBuilder {
@@ -34,17 +34,17 @@ impl LevelBuilder {
             lower_zero_index: 0,
             lower_one_index: number_of_zeros * index_of_bit_to_extract as usize,
             upper_bit_mask: 1<<index_of_bit_to_extract,
-            bits_per_value: index_of_bit_to_extract
+            bits_per_item: index_of_bit_to_extract
         }
     }
 
-    /// Adds subsequent `value` from previous level to `self`.
-    fn push(&mut self, value: u64) {
-        let is_one = value & self.upper_bit_mask != 0;
+    /// Adds subsequent `item` from previous level to `self`.
+    fn push(&mut self, item: u64) {
+        let is_one = item & self.upper_bit_mask != 0;
         self.upper_bit.init_successive_bit(&mut self.upper_index, is_one);
         self.lower_bits.init_successive_bits(
             if is_one { &mut self.lower_one_index } else { &mut self.lower_zero_index },
-            value & (self.upper_bit_mask-1), self.bits_per_value);
+            item & (self.upper_bit_mask-1), self.bits_per_item);
     }
 }
 
@@ -83,10 +83,10 @@ impl<S> Level<S> where S: Select0ForRank101111 {
     }
 }
 
-/// [`Sequence`] stores a sequence of `len` `bits_per_value`-bit values
-/// using just over (about 4%) `len * bits_per_value` bits and
-/// quickly (mostly in *O(bits_per_value)* time) executes many useful queries, such as:
-/// - *access* a value with a given index - see [`Self::get`],
+/// [`Sequence`] stores a sequence of `len` `bits_per_item`-bit items
+/// using just over (about 4%) `len * bits_per_item` bits and
+/// quickly (mostly in *O(bits_per_item)* time) executes many useful queries, such as:
+/// - *access* a item with a given index - see [`Self::get`],
 /// - *select* - see [`Self::select`],
 /// - *rank* - see [`Self::rank`].
 /// 
@@ -101,7 +101,8 @@ impl<S> Level<S> where S: Select0ForRank101111 {
 ///   Lecture Notes in Computer Science, vol 7608, Springer, Berlin, Heidelberg,
 ///   <https://doi.org/10.1007/978-3-642-34109-0_18>
 /// 
-/// Additionally, our implementation draws some ideas from the Go implementation by Daisuke Okanohara,
+/// Additionally, our implementation draws some ideas (like elimination of recursion)
+/// from the Go implementation by Daisuke Okanohara,
 /// available at <https://github.com/hillbig/waveletTree/>.
 pub struct Sequence<S = CombinedSampling> {
     levels: Box<[Level<S>]>,
@@ -109,42 +110,42 @@ pub struct Sequence<S = CombinedSampling> {
 }
 
 impl<S> Sequence<S> {
-    /// Returns number of stored values.
+    /// Returns number of stored items.
     #[inline] pub fn len(&self) -> usize { self.len }
 
     /// Returns whether the sequence is empty.
     #[inline] pub fn is_empty(&self) -> bool { self.len == 0 }
 
-    /// Returns the size of each value in bits.
-    #[inline] pub fn bits_per_value(&self) -> u8 { self.levels.len() as u8 }
+    /// Returns the size of each item in bits.
+    #[inline] pub fn bits_per_item(&self) -> u8 { self.levels.len() as u8 }
 }
 
 impl Sequence<CombinedSampling> {
-    /// Constructs [`Sequence`] with `content_len` `bits_per_value`-bit
-    /// values exposed by iterator returned by `content` function.
-    pub fn from_fn<I, F>(content: F, content_len: usize, bits_per_value: u8) -> Self
+    /// Constructs [`Sequence`] with `content_len` `bits_per_item`-bit
+    /// items exposed by iterator returned by `content` function.
+    pub fn from_fn<I, F>(content: F, content_len: usize, bits_per_item: u8) -> Self
         where I: IntoIterator<Item = u64>, F: FnMut() -> I {
-        Self::from_fn_s(content, content_len, bits_per_value)
+        Self::from_fn_s(content, content_len, bits_per_item)
     }
 
-    /// Constructs [`Sequence`] with `content` consisted of `content_len` `bits_per_value`-bit
-    /// values contained in the bit vector.
-    pub fn from_bits(content: &[u64], content_len: usize, bits_per_value: u8) -> Self {
-        Self::from_bits_s(content, content_len, bits_per_value)
+    /// Constructs [`Sequence`] with `content` consisted of `content_len` `bits_per_item`-bit
+    /// items contained in the bit vector.
+    pub fn from_bits(content: &[u64], content_len: usize, bits_per_item: u8) -> Self {
+        Self::from_bits_s(content, content_len, bits_per_item)
     }
 }
 
 impl<S> Sequence<S> where S: SelectForRank101111+Select0ForRank101111 {
 
-    /// Constructs [`Sequence`] with `content_len` `bits_per_value`-bit
-    /// values exposed by iterator returned by `content` function,
+    /// Constructs [`Sequence`] with `content_len` `bits_per_item`-bit
+    /// items exposed by iterator returned by `content` function,
     /// and custom select strategy.
-    pub fn from_fn_s<I, F>(mut content: F, content_len: usize, bits_per_value: u8) -> Self
+    pub fn from_fn_s<I, F>(mut content: F, content_len: usize, bits_per_item: u8) -> Self
         where I: IntoIterator<Item = u64>, F: FnMut() -> I
     {
-        assert!(bits_per_value > 0 && bits_per_value <= 63);
-        let mut levels = Vec::with_capacity(bits_per_value as usize);
-        if bits_per_value == 1 {
+        assert!(bits_per_item > 0 && bits_per_item <= 63);
+        let mut levels = Vec::with_capacity(bits_per_item as usize);
+        if bits_per_item == 1 {
             let mut level = Box::with_zeroed_bits(content_len);
             for (i, e) in content().into_iter().enumerate() {
                 level.init_bit(i, e != 0);
@@ -155,12 +156,12 @@ impl<S> Sequence<S> where S: SelectForRank101111+Select0ForRank101111 {
         let mut number_of_zeros = [0; 64];
         for mut e in content() {
             e = !e;
-            for zeros in &mut number_of_zeros[0..bits_per_value as usize] {
+            for zeros in &mut number_of_zeros[0..bits_per_item as usize] {
                 *zeros += (e & 1) as usize;
                 e >>= 1;
             }
         }
-        let mut current_bit = bits_per_value - 1;
+        let mut current_bit = bits_per_item - 1;
         let mut rest = {
             let mut level = LevelBuilder::new(
                 number_of_zeros[current_bit as usize], content_len, current_bit);
@@ -171,12 +172,12 @@ impl<S> Sequence<S> where S: SelectForRank101111+Select0ForRank101111 {
             level.lower_bits
         };
         while current_bit >= 2 {
-            let rest_bits_per_value = current_bit;
+            let rest_bits_per_item = current_bit;
             current_bit -= 1;
             let mut level = LevelBuilder::new(
                 number_of_zeros[current_bit as usize], content_len, current_bit);
-            for index in (0..content_len*rest_bits_per_value as usize).step_by(rest_bits_per_value as usize) {
-                level.push(rest.get_bits(index, rest_bits_per_value));
+            for index in (0..content_len*rest_bits_per_item as usize).step_by(rest_bits_per_item as usize) {
+                level.push(rest.get_bits(index, rest_bits_per_item));
             }
             rest = level.lower_bits;
             levels.push(Level::new(level.upper_bit, number_of_zeros[current_bit as usize]));
@@ -185,15 +186,15 @@ impl<S> Sequence<S> where S: SelectForRank101111+Select0ForRank101111 {
         Self { levels: levels.into_boxed_slice(), len: content_len }
     }
 
-    /// Constructs [`Sequence`] with `content` consisted of `content_len` `bits_per_value`-bit
-    /// values contained in the bit vector, and custom select strategy.
-    pub fn from_bits_s(content: &[u64], content_len: usize, bits_per_value: u8) -> Self {
+    /// Constructs [`Sequence`] with `content` consisted of `content_len` `bits_per_item`-bit
+    /// items contained in the bit vector, and custom select strategy.
+    pub fn from_bits_s(content: &[u64], content_len: usize, bits_per_item: u8) -> Self {
         Self::from_fn_s(
-            || { (0..content_len).map(|index| content.get_fragment(index, bits_per_value)) },
-             content_len, bits_per_value)
+            || { (0..content_len).map(|index| content.get_fragment(index, bits_per_item)) },
+             content_len, bits_per_item)
     }
 
-    /// Returns a value with given `index`. The result is undefined if `index` is out of bound.
+    /// Returns an item with given `index`. The result is undefined if `index` is out of bound.
     pub unsafe fn get_unchecked(&self, mut index: usize) -> u64 {
         let mut result = 0;
         for level in self.levels.iter() {
@@ -208,23 +209,23 @@ impl<S> Sequence<S> where S: SelectForRank101111+Select0ForRank101111 {
         result
     }
 
-    /// Returns a value with given `index` or [`None`] if `index` is out of bound.
+    /// Returns an item with given `index` or [`None`] if `index` is out of bound.
     #[inline] pub fn get(&self, index: usize) -> Option<u64> {
         (index < self.len()).then(|| unsafe {self.get_unchecked(index)})
     }
 
-    /// Returns a value with given `index` or panics if `index` is out of bound.
+    /// Returns an item with given `index` or panics if `index` is out of bound.
     #[inline] pub fn get_or_panic(&self, index: usize) -> u64 {
         self.get(index).expect("wavelet_matrix::Sequence::get index out of bound")
     }
 
-    /// Returns the number of `value` occurrences in the given `range`, or [`None`] if `range` is out of bound.
-    pub fn try_count_in_range(&self, mut range: std::ops::Range<usize>, value: u64) -> Option<usize> {
+    /// Returns the number of `item` occurrences in the given `range`, or [`None`] if `range` is out of bound.
+    pub fn try_count_in_range(&self, mut range: std::ops::Range<usize>, item: u64) -> Option<usize> {
         if self.len() < range.end { return None; }
-        let mut level_bit_mask = 1 << self.bits_per_value();
+        let mut level_bit_mask = 1 << self.bits_per_item();
         for level in self.levels.iter() {
             level_bit_mask >>= 1;
-            if value & level_bit_mask == 0 {
+            if item & level_bit_mask == 0 {
                 range.start = level.content.rank0(range.start);
                 range.end = level.content.rank0(range.end);
             } else {
@@ -235,54 +236,54 @@ impl<S> Sequence<S> where S: SelectForRank101111+Select0ForRank101111 {
         Some(range.len())
     }
 
-    /// Returns the number of `value` occurrences in the given `range`, or panics if `range` is out of bound.
-    pub fn count_in_range(&self, range: std::ops::Range<usize>, value: u64) -> usize {
-        self.try_count_in_range(range, value).expect("wavelet_matrix::Sequence::count_in_range range out of bound")
+    /// Returns the number of `item` occurrences in the given `range`, or panics if `range` is out of bound.
+    pub fn count_in_range(&self, range: std::ops::Range<usize>, item: u64) -> usize {
+        self.try_count_in_range(range, item).expect("wavelet_matrix::Sequence::count_in_range range out of bound")
     }
 
-    /// Returns the number of `value` occurrences before given `index`, or [`None`] if `index` is out of bound.
-    #[inline] pub fn try_rank(&self, index: usize, value: u64) -> Option<usize> {
-        self.try_count_in_range(0..index, value)
+    /// Returns the number of `item` occurrences before given `index`, or [`None`] if `index` is out of bound.
+    #[inline] pub fn try_rank(&self, index: usize, item: u64) -> Option<usize> {
+        self.try_count_in_range(0..index, item)
     }
 
-    /// Returns the number of `value` occurrences before the given `index`, or panics if `index` is out of bound.
-    #[inline] pub fn rank(&self, index: usize, value: u64) -> usize {
-        self.try_rank(index, value).expect("wavelet_matrix::Sequence::rank index out of bound")
+    /// Returns the number of `item` occurrences before the given `index`, or panics if `index` is out of bound.
+    #[inline] pub fn rank(&self, index: usize, item: u64) -> usize {
+        self.try_rank(index, item).expect("wavelet_matrix::Sequence::rank index out of bound")
     }
 
     /// The method from Claude-Navarro paper, used by select methods.
-    #[inline] fn sel(&self, rank: usize, value: u64, index: usize, level_nr: usize) -> Option<usize> {
+    #[inline] fn sel(&self, rank: usize, item: u64, index: usize, level_nr: usize) -> Option<usize> {
         let level = match self.levels.get(level_nr) {
             Some(level) => level,
             None => return Some(index + rank)
         };
-        if value & (1<<(self.levels.len()-level_nr-1)) == 0 {
+        if item & (1<<(self.levels.len()-level_nr-1)) == 0 {
             level.try_select0(
-                self.sel(rank, value, level.content.rank0(index), level_nr + 1)?,
+                self.sel(rank, item, level.content.rank0(index), level_nr + 1)?,
                 self.len
             )
         } else {
             level.try_select(
-                self.sel(rank, value, level.content.rank(index) + level.number_of_zeros, level_nr + 1)?
+                self.sel(rank, item, level.content.rank(index) + level.number_of_zeros, level_nr + 1)?
                 - level.number_of_zeros,
                 self.len
             )
         }
     }
 
-    /// Returns the index of the `rank`-th (counting from 0) occurrence of `value`
+    /// Returns the index of the `rank`-th (counting from 0) occurrence of `item`
     /// or [`None`] if there are not so many occurrences.
-    pub fn try_select(&self, rank: usize, value: u64) -> Option<usize> {
-        self.sel(rank, value, 0, 0)
+    pub fn try_select(&self, rank: usize, item: u64) -> Option<usize> {
+        self.sel(rank, item, 0, 0)
     }
 
-    /// Returns the index of the `rank`-th (counting from 0) occurrence of `value`
+    /// Returns the index of the `rank`-th (counting from 0) occurrence of `item`
     /// or panics if there are not so many occurrences.
-    #[inline] pub fn select(&self, rank: usize, value: u64) -> usize {
-        self.try_select(rank, value).expect("wavelet_matrix::Sequence::select: there are no rank occurrences of the value")
+    #[inline] pub fn select(&self, rank: usize, item: u64) -> usize {
+        self.try_select(rank, item).expect("wavelet_matrix::Sequence::select: there are no rank occurrences of the item")
     }
 
-    /// Returns iterator over all values.
+    /// Returns iterator over all items.
     pub fn iter(&self) -> impl Iterator<Item = u64> + DoubleEndedIterator + FusedIterator + '_ {
         (0..self.len()).map(|i| unsafe { self.get_unchecked(i) })
     }
@@ -301,7 +302,7 @@ mod tests {
     fn test_empty() {
         let wm = Sequence::from_bits(&[], 0, 2);
         assert_eq!(wm.len(), 0);
-        assert_eq!(wm.bits_per_value(), 2);
+        assert_eq!(wm.bits_per_item(), 2);
         assert_eq!(wm.get(0), None);
         assert_eq!(wm.rank(0, 0), 0);
         assert_eq!(wm.iter().next(), None);
@@ -311,7 +312,7 @@ mod tests {
     fn test_1_level() {
         let wm = Sequence::from_bits(&[0b1101], 4, 1);
         assert_eq!(wm.len(), 4);
-        assert_eq!(wm.bits_per_value(), 1);
+        assert_eq!(wm.bits_per_item(), 1);
         assert_eq!(wm.get(0), Some(1));
         assert_eq!(wm.get(1), Some(0));
         assert_eq!(wm.get(2), Some(1));
@@ -330,7 +331,7 @@ mod tests {
     fn test_2_levels() {
         let wm = Sequence::from_bits(&[0b01_01_10_11], 4, 2);
         assert_eq!(wm.len(), 4);
-        assert_eq!(wm.bits_per_value(), 2);
+        assert_eq!(wm.bits_per_item(), 2);
         assert_eq!(wm.get(0), Some(0b11));
         assert_eq!(wm.get(1), Some(0b10));
         assert_eq!(wm.get(2), Some(0b01));
@@ -348,7 +349,7 @@ mod tests {
     fn test_3_levels() {
         let wm = Sequence::from_bits(&[0b000_110], 2, 3);
         assert_eq!(wm.len(), 2);
-        assert_eq!(wm.bits_per_value(), 3);
+        assert_eq!(wm.bits_per_item(), 3);
         assert_eq!(wm.get(0), Some(0b110));
         assert_eq!(wm.get(1), Some(0b000));
         assert_eq!(wm.get(2), None);
@@ -358,7 +359,7 @@ mod tests {
     fn test_4_levels() {
         let wm = Sequence::from_bits(&[0b1101_1010_0001_0001_1011], 5, 4);
         assert_eq!(wm.len(), 5);
-        assert_eq!(wm.bits_per_value(), 4);
+        assert_eq!(wm.bits_per_item(), 4);
         assert_eq!(wm.get(0), Some(0b1011));
         assert_eq!(wm.get(1), Some(0b0001));
         assert_eq!(wm.get(2), Some(0b0001));
