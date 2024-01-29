d5b5cca4db0e8b28f8303d0c68221277392dfad4
Piotr Beling <piotr.beling@wmii.uni.lodz.pl>
2024-01-29 16:59:41 +0100

    reverse_code impl.

--- minimum_redundancy/src/degree.rs
+++ minimum_redundancy/src/degree.rs
@@ -25,10 +25,36 @@ pub trait TreeDegree: Sized + Copy + Mul<u32, Output=u32> {
     /// Returns the `fragment_nr`-th fragment of `bits`. Result is less than `self.tree_degree()`.
     fn get_fragment(&self, bits: u32, fragment_nr: u32) -> u32;
 
-    /// Appends the `fragment` (that must be less than `self.tree_degree`) to the lowest digits (bits) of `bits`.
+    /// Appends the `fragment` (that must be less than `self.tree_degree`)
+    /// to the lowest digits (bits) of `bits`.
     fn push_front(&self, bits: &mut u32, fragment: u32) {
         *bits = *self * *bits + fragment;
     }
+
+    /// Returns the largest number of fragments that can be explicitly stored in the code.
+    /// Longer codes begin with a sequence of zeros and only their last fragments are explicitly represented.
+    fn code_capacity(&self) -> u8;
+
+    fn reverse_code(&self, bits: u32, len: u32) -> u32;
+
+    //type MULTIPLIER; ??
+
+    //fn first_multiplier(&self) -> u32;
+
+    //fn copy_fragment_mult(&self, src_code: u32, src_divider: u32, dst_bits: &mut u32, dst_multiplier: u32);
+
+    //fn push_at_mult(&self, multiplier: u32, bits: &mut u32, fragment: u32);
+
+    //fn next_multiplier(multiplier: u32) -> u32; // multiplier + multiplier??
+    //fn inc_multiplier(multiplier: &mut u32);
+
+    // Returns the `fragment_nr`-th fragment of reversed `bits`.
+    // Result is less than `self.tree_degree()`.
+    //fn reversed_get_fragment(&self, bits: u32, len: u32, fragment_nr: u32) -> u32;
+
+    // Appends the `fragment` (that must be less than `self.tree_degree`)
+    // to the highest digits (bits) of `bits`.
+    //fn reversed_push_front(&self, bits: u32, len: u32, fragment_nr: u32);
 }
 
 /// `BitsPerFragment` represents the Huffman's tree degree that is the power of two.
@@ -71,6 +97,35 @@ impl TreeDegree for BitsPerFragment {
     fn push_front(&self, bits: &mut u32, fragment: u32) {
         *bits = *self * *bits | fragment;
     }
+
+    #[inline(always)] fn code_capacity(&self) -> u8 { 32 / self.0 }
+
+    #[inline] fn reverse_code(&self, mut bits: u32, len: u32) -> u32 {
+        //TODO faster code for self.0 == 1 or self.0 being power of 2
+        if self.0 == 1 {    // very common and the slowest case
+            return bits.reverse_bits() >> 32u32.saturating_sub(len);
+        }
+        let mut len = len.min(self.code_capacity() as u32) as u8;
+        let mask = self.as_u32() - 1;
+        let mut result = 0;
+        while bits != 0 {
+            result <<= self.0;
+            result |= bits & mask;
+            bits >>= self.0;
+            len -= 1;
+        }
+        result << (self.0 * len)
+    }
+
+    /*fn reversed_get_fragment(&self, bits: u32, len: u32, fragment_nr: u32) -> u32 {
+        todo!()
+        let capacity = 32 / self.0;
+        self.get_fragment(bits, len - fragment_nr)
+    }
+
+    fn reversed_push_front(&self, bits: u32, len: u32, fragment_nr: u32) {
+        todo!()
+    }*/
 }
 
 impl TryFrom<Degree> for BitsPerFragment {
@@ -108,13 +163,153 @@ impl TreeDegree for Degree {
         VByte::read(input).map(|v| Self(v))
     }
 
-    fn get_fragment(&self, bits: u32, fragment_nr: u32) -> u32 {
+    #[inline] fn get_fragment(&self, bits: u32, fragment_nr: u32) -> u32 {
         self.0.checked_pow(fragment_nr).map_or(0, |v| (bits/v) % self.0)
     }
+
+    #[inline] fn code_capacity(&self) -> u8 {
+        (1u64<<32).ilog(self.0 as u64) as u8
+    }
+    
+    #[inline] fn reverse_code(&self, mut bits: u32, mut len: u32) -> u32 {
+        //TODO use some lib for fast dividing
+        let mut result = 0;
+        let mut representable = ((1u64<<32) / self.0 as u64) as u32; // >0 only if we can add next digit to result
+        while bits != 0 {
+            result *= self.0;
+            result += bits % self.0;
+            bits /= self.0;
+            len -= 1;
+            if representable == 0 { return result; }
+            representable /= self.0;
+        }
+        while len != 0 && representable != 0 {
+            result *= self.0;
+            len -= 1;
+            representable /= self.0;
+        }        
+        result
+    }
+
+    
 }
 
 impl From<BitsPerFragment> for Degree {
     fn from(bits_per_fragment: BitsPerFragment) -> Self {
         Self(bits_per_fragment.as_u32())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn check_degree_2(d: impl TreeDegree) {
+        assert_eq!(d.get_fragment(0b1011, 0), 1);
+        assert_eq!(d.get_fragment(0b1011, 1), 1);
+        assert_eq!(d.get_fragment(0b1011, 2), 0);
+        assert_eq!(d.get_fragment(0b1011, 3), 1);
+        assert_eq!(d.get_fragment(0b1011, 4), 0);
+        assert_eq!(d.get_fragment(0b1011, 17), 0);
+        assert_eq!(d.get_fragment(0b1011, 31), 0);
+        assert_eq!(d.get_fragment(0b1011, 32), 0);
+        assert_eq!(d.get_fragment(0b1011, 33), 0);
+        assert_eq!(d.code_capacity(), 32);
+        assert_eq!(d.reverse_code(0b1011, 4), 0b1101);
+        assert_eq!(d.reverse_code(0b1011, 5), 0b11010);
+        assert_eq!(d.reverse_code(0b1011, 7), 0b1101000);
+        assert_eq!(d.reverse_code(0b1011, 31), 0b01101000_00000000_00000000_00000000);
+        assert_eq!(d.reverse_code(0b1011, 32), 0b11010000_00000000_00000000_00000000);
+        assert_eq!(d.reverse_code(0b1011, 33), 0b11010000_00000000_00000000_00000000);
+        assert_eq!(d.reverse_code(0b1011, 44), 0b11010000_00000000_00000000_00000000);
+    }
+
+    #[test]
+    fn bits_per_fragment_1() {
+        check_degree_2(BitsPerFragment(1))
+    }
+    
+    #[test]
+    fn degree_2() {
+        check_degree_2(Degree(2));
+    }
+
+    fn check_degree_4(d: impl TreeDegree) {
+        assert_eq!(d.get_fragment(0b10_00_11, 0), 0b11);
+        assert_eq!(d.get_fragment(0b10_00_11, 1), 0b00);
+        assert_eq!(d.get_fragment(0b10_00_11, 2), 0b10);
+        assert_eq!(d.get_fragment(0b10_00_11, 3), 0b00);
+        assert_eq!(d.get_fragment(0b10_00_11, 8), 0b00);
+        assert_eq!(d.get_fragment(0b10_00_11, 15), 0b00);
+        assert_eq!(d.get_fragment(0b10_00_11, 16), 0b00);
+        assert_eq!(d.get_fragment(0b10_00_11, 17), 0b00);
+        assert_eq!(d.get_fragment(0b10_00_11, 33), 0b00);
+        assert_eq!(d.code_capacity(), 16);
+        assert_eq!(d.reverse_code(0b10_00_11, 3), 0b11_00_10);
+        assert_eq!(d.reverse_code(0b10_00_11, 4), 0b11_00_10_00);
+        assert_eq!(d.reverse_code(0b10_00_11, 5), 0b11_00_10_00_00);
+        assert_eq!(d.reverse_code(0b10_00_11, 15), 0b11_00_10_00_00__00000_00000_00000_00000);
+        assert_eq!(d.reverse_code(0b10_00_11, 16), 0b11_00_10_00_00_00__00000_00000_00000_00000);
+        assert_eq!(d.reverse_code(0b10_00_11, 17), 0b11_00_10_00_00_00__00000_00000_00000_00000);
+        assert_eq!(d.reverse_code(0b10_00_11, 20), 0b11_00_10_00_00_00__00000_00000_00000_00000);
+    }
+
+    #[test]
+    fn bits_per_fragment_2() {
+        check_degree_4(BitsPerFragment(2));
+    }
+
+    #[test]
+    fn degree_4() {
+        check_degree_4(Degree(4));
+    }
+
+    fn check_degree_8(d: impl TreeDegree) {
+        assert_eq!(d.get_fragment(0b100_000_111, 0), 0b111);
+        assert_eq!(d.get_fragment(0b100_000_111, 1), 0b000);
+        assert_eq!(d.get_fragment(0b100_000_111, 2), 0b100);
+        assert_eq!(d.get_fragment(0b100_000_111, 3), 0b000);
+        assert_eq!(d.get_fragment(0b100_000_111, 10), 0b000);
+        assert_eq!(d.get_fragment(0b100_000_111, 20), 0b000);
+        assert_eq!(d.code_capacity(), 10);
+        assert_eq!(d.reverse_code(0b100_000_111, 3), 0b111_000_100);
+        assert_eq!(d.reverse_code(0b100_000_111, 4), 0b111_000_100_000);
+        assert_eq!(d.reverse_code(0b100_000_111, 5), 0b111_000_100_000_000);
+        assert_eq!(d.reverse_code(0b100_000_111, 9), 0b111_000_100_000_000_000_000_000_000);
+        assert_eq!(d.reverse_code(0b100_000_111, 10), 0b111_000_100_000_000_000_000_000_000_000);
+        assert_eq!(d.reverse_code(0b100_000_111, 11), 0b111_000_100_000_000_000_000_000_000_000);
+        assert_eq!(d.reverse_code(0b100_000_111, 12), 0b111_000_100_000_000_000_000_000_000_000);
+        assert_eq!(d.reverse_code(0b100_000_111, 19), 0b111_000_100_000_000_000_000_000_000_000);
+    }
+    
+    #[test]
+    fn bits_per_fragment_3() {
+        check_degree_8(BitsPerFragment(3));
+    }
+
+    #[test]
+    fn degree_8() {
+        check_degree_8(Degree(8));
+    }
+
+    #[test]
+    fn degree_27() {
+        let d = Degree(27);
+        let bits = 19 * (27*27) + 5 * 27 + 26;
+        assert_eq!(d.get_fragment(bits, 0), 26);
+        assert_eq!(d.get_fragment(bits, 1), 5);
+        assert_eq!(d.get_fragment(bits, 2), 19);
+        assert_eq!(d.get_fragment(bits, 3), 0);
+        assert_eq!(d.get_fragment(bits, 5), 0);
+        assert_eq!(d.get_fragment(bits, 6), 0);
+        assert_eq!(d.get_fragment(bits, 11), 0);
+        assert_eq!(d.code_capacity(), 6);
+        let reversed = 26 * (27*27) + 5 * 27 + 19;
+        assert_eq!(d.reverse_code(bits, 3), reversed);
+        assert_eq!(d.reverse_code(bits, 4), reversed * 27);
+        assert_eq!(d.reverse_code(bits, 5), reversed * 27 * 27);
+        assert_eq!(d.reverse_code(bits, 6), reversed * 27 * 27 * 27);
+        assert_eq!(d.reverse_code(bits, 7), reversed * 27 * 27 * 27);
+        assert_eq!(d.reverse_code(bits, 11), reversed * 27 * 27 * 27);
     }
 }
