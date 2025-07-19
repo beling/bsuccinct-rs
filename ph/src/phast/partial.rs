7f3d11d20b4db54fb22bc353b00b3251370928d8
Piotr Beling <piotr.beling@wmii.uni.lodz.pl>
2025-07-19 18:03:55 +0200

    work on BuildStats

--- ph/src/phast/partial.rs
+++ ph/src/phast/partial.rs
@@ -2,7 +2,74 @@ use dyn_size_of::GetSize;
 use voracious_radix_sort::RadixSort;
 
 use crate::{phast::{builder::{build_mt, build_st, BuildConf}, conf::Conf, evaluator::BucketToActivateEvaluator, function::SeedEx, Params, SeedChooser, SeedOnly, WINDOW_SIZE}, seeds::SeedSize};
-use std::hash::{BuildHasher, Hash, RandomState};
+use std::{hash::{BuildHasher, Hash, RandomState}, time};
+
+
+pub trait BuildStats: Copy {
+    #[inline(always)] fn pre_hash(&mut self) {}
+    #[inline(always)] fn pre_sort(&mut self) {}
+    #[inline(always)] fn pre_seeding(&mut self) {}
+    #[inline(always)] fn pre_keys_removing(&mut self) {}
+    #[inline(always)] fn post_keys_removing(&mut self) {}
+}
+
+impl BuildStats for () {}
+
+#[derive(Clone, Copy)]
+pub struct BuildProgressRaport {
+    timer: time::Instant,
+    hash: time::Duration,
+    sort: time::Duration,
+    seeding: time::Duration,
+    removing: time::Duration
+}
+
+impl Default for BuildProgressRaport {
+    fn default() -> Self {
+        Self { timer: time::Instant::now(), hash: Default::default(), sort: Default::default(), seeding: Default::default(), removing: Default::default() }
+    }
+}
+
+impl BuildStats for BuildProgressRaport {
+    fn pre_hash(&mut self) {
+        print!("Calculating primary hashes... ");
+        self.timer = time::Instant::now();
+    }
+
+    fn pre_sort(&mut self) {
+        self.hash = self.timer.elapsed();
+        println!("DONE in {:#.2?}", self.hash);
+        print!("Sorting...");
+        self.timer = time::Instant::now();
+    }
+
+    fn pre_seeding(&mut self) {
+        self.sort = self.timer.elapsed();
+        println!("DONE in {:#.2?}", self.sort);
+        print!("Calculating seeds...");
+        self.timer = time::Instant::now();
+    }
+
+    fn pre_keys_removing(&mut self) {
+        self.seeding = self.timer.elapsed();
+        println!("DONE in {:#.2?}", self.seeding);
+        print!("Removing assigned keys...");
+        self.timer = time::Instant::now();
+    }
+
+    fn post_keys_removing(&mut self) {
+        self.removing = self.timer.elapsed();
+        println!("DONE in {:#.2?}", self.removing);
+        let total = self.hash + self.sort + self.seeding + self.removing;
+        println!("{total:#.2?} total with {:.0}/{:.0}/{:.0}/{:.0} percentage shares",
+            self.hash.div_duration_f64(total) * 100.0,
+            self.sort.div_duration_f64(total) * 100.0,
+            self.seeding.div_duration_f64(total) * 100.0,
+            self.removing.div_duration_f64(total) * 100.0,
+        )
+    }
+}
+
 
 /// Map-or-bump function that assigns different numbers to some keys and `None` to other.
 pub struct Partial<SS, SC = SeedOnly, S = RandomState> where SS: SeedSize {
