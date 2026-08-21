10a973b0a1f01fc04fc13b38dd4c210da6745aa9
Piotr Beling <piotr.beling@wmii.uni.lodz.pl>
2026-08-21 11:05:32 +0200

    simpler func names

--- internal/asmview/src/lib.rs
+++ internal/asmview/src/lib.rs
@@ -4,16 +4,21 @@ use ph::phast::{Function, Function2, GenericCore, ShiftWrappedCore, SeedCore};
 use ph::seeds::Bits8;
 
 #[no_mangle]
-pub extern "C" fn phast_get_f2_shift_only_wrapped_m1(f: &Function2<GenericCore, Bits8, ShiftWrappedCore>, key: u64) -> usize {
+pub extern "C" fn phast_get_f2_shift_wrap_m1(f: &Function2<GenericCore, Bits8, ShiftWrappedCore>, key: u64) -> usize {
     f.get(&key)
 }
 
 #[no_mangle]
-pub extern "C" fn phast_get_f2_seed_only(f: &Function2<GenericCore, Bits8, SeedCore>, key: u64) -> usize {
+pub extern "C" fn phast_get_f2_shift_wrap_m3(f: &Function2<GenericCore, Bits8, ShiftWrappedCore<3>>, key: u64) -> usize {
     f.get(&key)
 }
 
 #[no_mangle]
-pub extern "C" fn phast_get_f_seed_only(f: &Function<GenericCore, Bits8, SeedCore>, key: u64) -> usize {
+pub extern "C" fn phast_get_f2_seed(f: &Function2<GenericCore, Bits8, SeedCore>, key: u64) -> usize {
+    f.get(&key)
+}
+
+#[no_mangle]
+pub extern "C" fn phast_get_f_seed(f: &Function<GenericCore, Bits8, SeedCore>, key: u64) -> usize {
     f.get(&key)
 }
