1c7d01e22630cd6fcfa1a4d4339bd18239ecd360
Piotr Beling <piotr.beling@wmii.uni.lodz.pl>
2026-08-31 21:28:38 +0200

    weights

--- ph/src/phast/seed_chooser/k.rs
+++ ph/src/phast/seed_chooser/k.rs
@@ -310,7 +310,7 @@ impl<SE: KSeedEvaluator> SeedChooser for SeedOnlyK<SE> {
 
 
 type P=ProdOfValuesKEval;
-const PROD_S8_L512: [(u16, [i32; 7], ProdOfValuesKEval); 8] = [   // for W=512
+const PROD_S8_L512: [(u16, [i32; 7], ProdOfValuesKEval); 10] = [   // for W=512
     (2, [0, 76473, 93538, 100036, 111405, 124459, 127558], P{value_shift: 0.00968, free_shift: 1.94309, first_weight: 0.28871}), // 1.28% for λ=6.88
     (4, [0, 124831, 125295, 125906, 134811, 144356, 145403], P{value_shift: 0.00668, free_shift: 1.29751, first_weight: 0.27419}), // 1.12% for λ=11.02
     (6, [0, 149127, 160970, 171446, 177056, 182860, 183392], P{value_shift: 0.005145, free_shift: 1.194641, first_weight: 0.584520}), //0.99% for λ=14.76
@@ -318,7 +318,9 @@ const PROD_S8_L512: [(u16, [i32; 7], ProdOfValuesKEval); 8] = [   // for W=512
     (12, [0, 169779, 209752, 213158, 213291, 213433, 213442], P{value_shift: 0.003807, free_shift: 1.141909, first_weight: 0.857620}), // 0.86% for λ=24.90
     (16, [0, 170107, 206229, 210576, 210830, 211146, 211180], P{value_shift: 0.00365, free_shift: 1.12751, first_weight: 0.87378}), // 0.85% for λ=31.16
     (20, [0, 172926, 194515, 207725, 209558, 211778, 211993], P{value_shift: 0.003568, free_shift: 1.125351, first_weight: 0.905329}), // 0.85% for λ=37.17
-    (24, [0, 179037, 198479, 210605, 212759, 215360, 215624], P{value_shift: 0.003623, free_shift: 1.118238, first_weight: 0.914403}) // 0.83% for λ=43.00
+    (24, [0, 179037, 198479, 210605, 212759, 215360, 215624], P{value_shift: 0.003623, free_shift: 1.118238, first_weight: 0.914403}), // 0.83% for λ=43.00
+    (32, [0, 179706, 199221, 210865, 213144, 215884, 216163], P{value_shift: 0.003632, free_shift: 1.128020, first_weight: 0.916687}), // 0.79% for λ=54.24
+    (48, [0, 179533, 198080, 208200, 210344, 212994, 213256], P{value_shift: 0.003635, free_shift: 1.173609, first_weight: 0.925055}), // 0.77% for λ=75.62
 ];
 #[cfg(not(feature = "W256"))]
 const PROD_S8_L1024: [(u16, [i32; 7], ProdOfValuesKEval); 33] = [   // for W=512
